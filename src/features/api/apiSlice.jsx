456e51a6e28a49497117e67504c79854b73f3678
Arijit Banarjee <ontikdev.2@gmail.com>
2023-07-06 16:43:20 +0600

    Add login functionalities

--- src/features/api/apiSlice.jsx
+++ src/features/api/apiSlice.jsx
@@ -1,18 +1,33 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { userLoggedOut } from "../auth/authSlice";
+
+/**
+ * base query
+ */
+const baseQuery = fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_REACT_APP_API__URL,
+    prepareHeaders: async (headers, { getState, endpoints }) => {
+        const token = getState()?.auth?.accessToken;
+        if (token) {
+            headers.set("Authorization", `Bearer ${token}`);
+        }
+
+        return headers;
+    }
+});
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_REACT_APP_API__URL,
-        prepareHeaders: async (headers, { getState, endpoints }) => {
-            const token = getState()?.auth?.accessToken;
-            if (token) {
-                headers.set("Authorization", `Bearer ${token}`);
-            }
+    baseQuery: async (args, api, extraOptions) => {
+        let result = await baseQuery(args, api, extraOptions);
 
-            return headers;
+        if (result?.error?.status === 401) {
+            api.dispatch(userLoggedOut());
+            localStorage.removeItem(import.meta.env.VITE_APP_NAME);
         }
-    }),
+
+        return result;
+    },
     tagTypes: [],
     endpoints: (builder) => ({})
 });
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     /**
      * login response hook
      */
-    const [login, { data, isLoading, error: responseError }] = useLoginMutation();
+    const [login, { data, isLoading, isError, error: responseError }] = useLoginMutation();
 
     /**
      * 
@@ -62,7 +62,7 @@ const Login = () => {
      */
     useEffect(() => {
         if (responseError?.data) {
-            setError(responseError.data);
+            setError(responseError.data.errors);
         }
 
         if (data?.accessToken && data?.user) {
@@ -75,31 +75,26 @@ const Login = () => {
      */
     const handleOnSubmit = (e) => {
         e.preventDefault();
-
-        setError("");
-
-        if (form?.login == '' && form?.register == '') {
-            setError("Fill the form correctly!");
-        } else {
-            login({
-                email: form?.email,
-                password: form?.password
-            })
-        }
+        login({
+            email: form?.email,
+            password: form?.password
+        })
     }
 
     return (
         <div className="mt-5 rounded-md border-2 border-solid border-gray-200 w-[400px] mx-auto py-10 px-5">
             <h1 className="text-2xl font-semibold mb-5">Login</h1>
-            {error && <Error message={error} />}
+            {error?.common && <Error message={error?.common} />}
             <form onSubmit={handleOnSubmit}>
                 <label htmlFor="email" className="mb-2 block">Email <span className="text-red-600">*</span></label>
-                <input type="text" onChange={handleOnChange} value={form.email} name="email" id="email" className="border border-solid border-gray-200 rounded-sm block w-full px-3 py-2 transition delay-75 focus:outline-0 focus:border-gray-300" />
+                <input type="text" onChange={handleOnChange} value={form.email} name="email" id="email" className={`border border-solid ${error?.email ? 'border-red-600' : 'border-gray-200'} rounded-sm block w-full px-3 py-2 transition delay-75 focus:outline-0 ${error?.email ? 'focus:border-red-700' : 'focus:border-gray-300'}`} />
+                {error?.email && <span className="text-red-600 mt-1 block">{error?.email?.msg}</span>}
                 <label htmlFor="password" className="mb-2 block mt-3">Password <span className="text-red-600">*</span></label>
                 <div className="relative">
-                    <input type={toggle ? 'password' : 'text'} onChange={handleOnChange} value={form.password} name="password" id="password" className="border border-solid border-gray-200 rounded-sm block w-full px-3 py-2 transition delay-75 focus:outline-0 focus:border-gray-300" />
+                    <input type={toggle ? 'password' : 'text'} onChange={handleOnChange} value={form.password} name="password" id="password" className={`border border-solid ${error?.password ? 'border-red-600' : 'border-gray-200'} rounded-sm block w-full px-3 py-2 transition delay-75 focus:outline-0 ${error?.password ? 'focus:border-red-700' : 'focus:border-gray-300'}`} />
                     <FontAwesomeIcon icon={toggle ? faEye : faEyeSlash} className="absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer" onClick={togglePassword} />
                 </div>
+                {error?.password && <span className="text-red-600 mt-1 block">{error?.password?.msg}</span>}
                 <button type="submit" className="bg-blue-500 block mt-3 px-3 py-2 text-white font-semibold rounded-sm w-full focus:outline-none">Login</button>
             </form>
             <p className="text-dark mt-3">Have no account? <Link to="/register" className="text-blue-500 font-semibold">Register</Link></p>
