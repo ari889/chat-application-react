e8a3c3544c14b12f4b56e2a8155f32f4410a6bef
Arijit Banarjee <ontikdev.2@gmail.com>
2023-07-11 14:34:41 +0600

    Resolve pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./layout/Layout"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
-import Index from "./pages"
 import useAuthCheck from "./hooks/useAuthcheck"
 import PublicRoute from "./components/PublicRoute"
 import PrivateRoute from "./components/PrivateRoute"
@@ -14,14 +13,14 @@ function App() {
     <div>Checking authenticatyion...</div>
   ) : (
     <BrowserRouter>
-      {/* <Layout> */}
-      <Routes>
-        <Route path="/inbox" element={<PrivateRoute><Index /></PrivateRoute>} />
-        <Route path="/inbox/:id" element={<PrivateRoute><Index /></PrivateRoute>} />
-        <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-        <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
-      </Routes>
-      {/* </Layout> */}
+      <Layout>
+        <Routes>
+          <Route path="/inbox" element={<PrivateRoute><Index /></PrivateRoute>} />
+          <Route path="/inbox/:id" element={<PrivateRoute><Index /></PrivateRoute>} />
+          <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+          <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
