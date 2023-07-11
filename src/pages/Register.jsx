c96c428896c0fe66567be5113b7fb36937bf41ac
Arijit Banarjee <ontikdev.2@gmail.com>
2023-07-11 14:27:55 +0600

    Finalization

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { useRegisterMutation } from "../features/auth/authApi";
 import Error from "../ui/Error";
-import isValidEmail from "../utils/isValidEmail";
 
 const Register = () => {
     /**
